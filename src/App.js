bd4294cf684d7bc019bbbd06d018e85261f652c4
Byungchan Lim <bl458@cornell.edu>
2022-05-03 01:19:25 -0400

    FEAT create more html elements, components

--- src/App.js
+++ src/App.js
@@ -11,11 +11,21 @@ function App() {
       </div>
 
       <div className="body">
-        <div className="body_inner"></div>
+        <div className="body_inner">
+          <div className="body_my-photo-wrap"></div>
+          <div className="body_intro-wrap">
+            <p>
+              Hi, I am Michael, an incoming Cornell CS grad. I have a passion
+              for web dev and data science. I am currently exploring ideas in
+              web3. My hobbies are playing the piano and biking. Check out my
+              past projects and feel free to connect!
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className="footer">
-        <p>2020 Michael Lim</p>
+        <p>© 2020 Michael Lim</p>
         <a href="https://github.com/bl458/Prof-website">Source</a>
       </div>
     </div>
