1a71253dda091d7599b1ef0b4bf21684ac872dfb
Byungchan Lim <bl458@cornell.edu>
2022-05-03 19:11:31 -0400

    FEAT style header, footer

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
+import "./App.css";
+
 function App() {
   return (
     <div className="app">
       <div className="header">
-        <p className="title">michaellim</p>
+        <p className="header_title">michaellim</p>
         <div className="header_tabs">
-          <p>About</p>
-          <p>Projects</p>
-          <p>Writing</p>
+          <p className="header_tabs-text">About</p>
+          <p className="header_tabs-text">Projects</p>
+          <p className="header_tabs-text">Writing</p>
         </div>
       </div>
 
-      <div className="body">
-        <div className="body_inner">
-          <div className="body_my-photo-wrap"></div>
-          <div className="body_intro-wrap">
-            <p>
-              Hi, I am Michael, an incoming Cornell CS grad. I have a passion
-              for web dev and data science. I am currently exploring ideas in
-              web3. My hobbies are playing the piano and biking. Check out my
-              past projects and feel free to connect!
-            </p>
-          </div>
-        </div>
-      </div>
+      <div className="body"></div>
 
       <div className="footer">
-        <p>© 2020 Michael Lim</p>
-        <a href="https://github.com/bl458/Prof-website">Source</a>
+        <p className="footer_name">© 2020 Michael Lim</p>
+        <a
+          className="footer_source"
+          href="https://github.com/bl458/Prof-website"
+        >
+          Source
+        </a>
       </div>
     </div>
   );
