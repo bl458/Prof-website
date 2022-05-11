c47066ebbcbaf879cff8ea43fef340d43838a23f
Byungchan Lim <bl458@cornell.edu>
2022-05-11 10:08:51 -0400

    FEAT implement contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,44 @@
 import "./Contact.css";
 
 export default function Contact() {
-  return <div>This is the contact page.</div>;
+  return (
+    <div className="contact">
+      <p className="contact_text">
+        I'd love to hear from you!
+        <br />
+        <br />
+        The best way to contact me is to shoot me an email at{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="mailto: bl458@cornell.edu"
+          className="contact_link"
+        >
+          byungchan9707@gmail.com
+        </a>
+        .
+        <br />
+        <br />
+        You can also check out my{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/bl458"
+          className="contact_link"
+        >
+          github
+        </a>{" "}
+        or message me on{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/michael-byungchan-lim-629889126/"
+          className="contact_link"
+        >
+          Linkedin
+        </a>
+        .
+      </p>
+    </div>
+  );
 }
